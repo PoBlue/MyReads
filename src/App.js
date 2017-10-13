51b49eeb3a7edd16b5e95bc44d02ae653f63c26d
PoBlue <250299430@qq.com>
2017-10-13 18:33:27 +0800

    fixed: add book to shelf

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import SearchBook from './component/SearchBook';
 
 class BooksApp extends React.Component {
   state = {
-    books: null,
+    books: [],
   }
 
-  componentWillMount() {
+  componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books: books });
     });
@@ -21,14 +21,20 @@ class BooksApp extends React.Component {
 
   updateBook(bookToUpdate) {
     const { books } = this.state;
+    let haveBook = false;
 
     let updatedBooks = books.map((book) => {
       if (book.id === bookToUpdate.id) {
+        haveBook = true;
         return bookToUpdate;
       } else {
         return book;
       }
     });
+
+    if(!haveBook){
+      updatedBooks.push(bookToUpdate);
+    }
 
     BooksAPI.update(bookToUpdate, bookToUpdate.shelf).then(
       this.setState({ books: updatedBooks })
